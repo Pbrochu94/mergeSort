7d32bc3565c866654c75e49d80afccee5801bc95
Patrick Brochu <patrickbrochu70@gmail.com>
2025-05-17 14:52:38 -0400

    Iterative fibonaccy done
    TODO: Recursive fibonaccy

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+function fibs(num) {
+  let array = [];
+  for (let x = 0; x < num; x++) {
+    if (x <= 1) {
+      array.push(x);
+    } else {
+      array.push(array[x - 1] + array[x - 2]);
+    }
+  }
+  console.log(array);
+}
+
+function fibsRec(num) {}
+
+fibsRec(8);
+fibs(8);
