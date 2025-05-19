7d2ae3fc1ea293b2db6771d8ab2fe1900640a577
Patrick Brochu <patrickbrochu70@gmail.com>
2025-05-19 15:47:10 -0400

    Done with iterative and recurseive fibonacci sequence
    TODO: Mergesort

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ function fibs(num) {
   let array = [];
   for (let x = 0; x < num; x++) {
     if (x <= 1) {
-      array.push(x);
+      array.push(1);
     } else {
       array.push(array[x - 1] + array[x - 2]);
     }
   }
-  console.log(array);
+  return array;
 }
 
-function fibsRec(num) {}
+function fibsRec(num) {
+  if (num <= 1) {
+    return num;
+  } else {
+    return fibsRec(num - 1) + fibsRec(num - 2);
+  }
+}
 
-fibsRec(8);
-fibs(8);
+console.log(fibsRec(5));
+console.log(fibs(5));
