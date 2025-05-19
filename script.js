4783dcf480ec07053a40a642a974464eb2ed15b2
Patrick Brochu <patrickbrochu70@gmail.com>
2025-05-19 16:34:04 -0400

    Started the mergeSort function
    TODO:Keep working on it

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*Fibonacci*/
 function fibs(num) {
   let array = [];
   for (let x = 0; x < num; x++) {
@@ -18,5 +19,27 @@ function fibsRec(num) {
   }
 }
 
-console.log(fibsRec(5));
-console.log(fibs(5));
+/**Merge sort */
+
+let array = [3, 2, 1, 13, 8, 5, 0, 1, 2];
+mergeSort(array);
+
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return;
+  }
+  if (arr.length === 2) {
+    if (arr[0] > arr[1]) {
+      let placeHolder = arr[0];
+      arr[0] = arr[1];
+      arr[1] = placeHolder;
+      console.log(arr);
+      return arr;
+    }
+    return;
+  }
+  let middleIndex = Math.ceil(arr.length / 2); //Find the middle index to seperate the array
+  let leftHalf = arr.splice(0, middleIndex);
+  mergeSort(arr);
+  mergeSort(leftHalf);
+}
